d44034531b25ae6621dd3364a1c3a67742fa9677
lechinal <alin.lechintan@outlook.com>
2023-10-07 14:29:59 +0300

    up

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😎 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={event => setQuantity(Number(event.target.value))}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map(num => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={event => setDescription(event.target.value)}
+      ></input>
       <button>Add</button>
     </form>
   );
